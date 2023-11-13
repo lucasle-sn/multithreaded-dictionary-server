bdabfa1c160d43d42382699548089bce2fc66f86
lucasle-sn <lucas.le@swift-nav.com>
2023-11-13 16:57:50 +1100

    Add Dictionary server

---
+++ MultiThreadsDictionaryServer/DictionaryService.java
@@ -0,0 +1,16 @@
+/*
+ *    The University of Melbourne
+ *    School of Computing and Information Systems
+ *    COMP90015 Distributed System
+ *    Semester 01/2020
+ *
+ *    Author: QUANG TRUNG LE - 987445
+ */
+
+import org.json.JSONObject;
+
+public interface DictionaryService {
+    public JSONObject searching(String vocabulary);
+    public JSONObject adding (String vocabulary, String meaning);
+    public JSONObject removing (String vocabulary);
+}
